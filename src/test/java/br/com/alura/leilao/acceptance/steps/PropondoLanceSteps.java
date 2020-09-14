687f39dcbb2fe319bb659560101d4c206f7e6fb0
Lucas Novaes Dragojevic <lucas.dragojevic@gmail.com>
2020-09-13 21:51:24 -0300

    Criados testes com listas de parâmetros

--- src/test/java/br/com/alura/leilao/acceptance/steps/PropondoLanceSteps.java
+++ src/test/java/br/com/alura/leilao/acceptance/steps/PropondoLanceSteps.java
@@ -4,10 +4,13 @@ import static org.junit.Assert.assertEquals;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import br.com.alura.leilao.model.Lance;
 import br.com.alura.leilao.model.Leilao;
 import br.com.alura.leilao.model.Usuario;
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.Before;
 import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.Entao;
@@ -58,5 +61,32 @@ public class PropondoLanceSteps {
 		assertEquals(this.lances.size(), leilao.getLances().size());
 		assertEquals(this.lances.get(0).getValor(), leilao.getLances().get(0).getValor());
 		assertEquals(this.lances.get(1).getValor(), leilao.getLances().get(1).getValor());
+	}
+	
+	@Dado("um lance invalido de {double} reais")
+	public void um_lance_invalido_de_reais(Double valor) {
+		this.lance = new Lance(new BigDecimal(valor));
+	}
+
+	@Entao("o lance nao eh aceito")
+	public void o_lance_nao_eh_aceito() {
+		assertEquals(0, leilao.getLances().size());
+	}
+	
+	@Entao("o segundo lance nao eh aceito")
+	public void o_segundo_lance_nao_eh_aceito() {
+		assertEquals(1, leilao.getLances().size());
+		assertEquals(this.lances.get(0).getValor(), leilao.getLances().get(0).getValor());
+	}
+	
+	@Dado("dois lances")
+	public void dois_lances(DataTable dataTable) {
+		List<Map<String, String>> mapas = dataTable.asMaps();
+		for (Map<String, String> mapa : mapas) {
+			String valor = mapa.get("valor");
+			String nome = mapa.get("nome");
+			Lance lance = new Lance(new Usuario(nome), new BigDecimal(valor));
+			lances.add(lance);
+		}
 	}
 }
--- src/test/resources/features/propondo_lances.feature
+++ src/test/resources/features/propondo_lances.feature
@@ -12,3 +12,20 @@ Cenario: Propondo varios lances validos
 	E um lance de 15.0 reais do usuario "beltrano"
 	Quando propoe varios lances ao leilao
 	Entao os lances sao aceitos
+	
+Esquema do Cenario: Propondo um lance invalido
+	Dado um lance invalido de <valor> reais
+	Quando propoe ao leilao
+	Entao o lance nao eh aceito
+	Exemplos: 
+      | valor |
+      |   0   |
+      |  -1   |
+
+Cenario: Propondo uma sequencia de lances
+	Dado dois lances
+			| valor | nome   |
+      | 10.0  | fulano |
+      | 15.0  | fulano |
+	Quando propoe varios lances ao leilao
+	Entao o segundo lance nao eh aceito
